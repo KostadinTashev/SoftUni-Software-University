2c0c6d05c84ba6b096b9bf5dc153b8e87b6545de
KostadinTashev <kostadin.tashev2002@gmail.com>
2023-09-19 16:24:38 +0300

    First lecture and exercise tasks

---
+++ Python DB/PostgreSQL/PostgreSQL Introduction. Data Types. Working with Tables/02.insert_data_in_tables.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees
+    (first_name, last_name, hiring_date, salary, devices_number)
+VALUES
+	('Ivan', 'Ivanov', '22-01-09',1000.10, 442211),
+    ('Petar', 'Petrov', '21-11-11',1420.33, 440011),
+    ('Georgi', 'Georgiev', '23-01-01',1810.77, 443311);
