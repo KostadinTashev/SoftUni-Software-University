ab5879dc3b7ead05f1c45723553d6656a91a9fc3
KostadinTashev <kostadin.tashev2002@gmail.com>
2023-09-26 11:33:28 +0300

    Data Aggregation - Lab

--- Python DB/PostgreSQL/Data Aggregation - Lab/08.department_names.sql
+++ Python DB/PostgreSQL/Data Aggregation - Lab/08.department_names.sql
@@ -8,9 +8,26 @@ SELECT
 		WHEN department_id = 1 THEN 'Management'
 		WHEN department_id = 2 THEN 'Kitchen Staff'
 		WHEN department_id = 3 THEN 'Service Staff'
-		WHEN department_id > 3 THEN 'Other'
+		ELSE 'Other'
 	END AS "department_name"
 FROM 
 	employees
 ORDER BY
 	id;
+
+-- SELECT 
+-- 	id,
+-- 	first_name,
+-- 	last_name,
+-- 	trunc(salary, 2) AS salary,
+-- 	department_id,
+-- 	CASE department_id
+-- 		WHEN 1 THEN 'Management'
+-- 		WHEN 2 THEN 'Kitchen Staff'
+-- 		WHEN 3 THEN 'Service Staff'
+-- 		ELSE 'Other'
+-- 	END AS "department_name"
+-- FROM 
+-- 	employees
+-- ORDER BY
+-- 	id;
