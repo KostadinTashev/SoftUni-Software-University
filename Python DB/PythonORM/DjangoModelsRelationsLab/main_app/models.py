0ed742bd98b6d7c8b623417249d1479c6623ec13
KostadinTashev <kostadin.tashev2002@gmail.com>
2023-11-07 18:03:07 +0200

    Django Models Relations - Lab

---
+++ Python DB/PythonORM/DjangoModelsRelationsLab/main_app/models.py
@@ -0,0 +1,107 @@
+from django.db import models
+from datetime import date
+
+
+class Lecturer(models.Model):
+    first_name = models.CharField(
+        max_length=100
+    )
+
+    last_name = models.CharField(
+        max_length=100
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Subject(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+
+    code = models.CharField(
+        max_length=100
+    )
+
+    lecturer = models.ForeignKey(
+        Lecturer,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+
+
+class Student(models.Model):
+    student_id = models.CharField(
+        max_length=10,
+        primary_key=True
+    )
+
+    first_name = models.CharField(
+        max_length=100
+    )
+
+    last_name = models.CharField(
+        max_length=100
+    )
+
+    birth_date = models.DateField()
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    subjects = models.ManyToManyField(
+        Subject
+    )
+
+
+class StudentEnrollment(models.Model):
+    GRADE_CHOICES = (
+        ("A", "A"),
+        ("B", "B"),
+        ("C", "C"),
+        ("D", "D"),
+        ("F", "F")
+    )
+
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.CASCADE
+    )
+
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.CASCADE
+    )
+
+    enrollment_date = models.DateField(
+        default=date.today()
+    )
+
+    grade = models.CharField(
+        max_length=1,
+        choices=GRADE_CHOICES
+    )
+
+
+class LecturerProfile(models.Model):
+    lecturer = models.OneToOneField(
+        Lecturer,
+        on_delete=models.CASCADE
+    )
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    bio = models.TextField(
+        null=True,
+        blank=True
+    )
+
+    office_location = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
