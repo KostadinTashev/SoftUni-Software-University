cf370f7b532662e75981e58941bab8cab2220c1d
KostadinTashev <kostadin.tashev2002@gmail.com>
2024-02-28 16:22:24 +0200

    Forms

---
+++ Python Web/forms_demos/forms_demos/web/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from forms_demos.web.views import index_form, index_model_forms
+
+urlpatterns = (
+    path('', index_form, name='index'),
+    path('model-forms/', index_model_forms, name='model forms')
+)
