52ebcb0cbb4ac398d102e0fb8c4c9f38941af3a7
Kostadin Tashev <kostadin.tashev2002@gmail.com>
2024-06-21 22:22:04 +0300

    Drawing Figures with Loops - More Exercises

--- Programming Basics with Python/Drawing Figures with Loops - More Exercises/05_square_frame.py
+++ Programming Basics with Python/Drawing Figures with Loops - More Exercises/05_square_frame.py
@@ -1,8 +1,5 @@
 n = int(input())
-
 print(f"+{' -' * (n - 2)} +")
-
 for rows in range(n - 2):
-   print(f"|{' -' * (n - 2)} |")
-
+    print(f"|{' -' * (n - 2)} |")
 print(f"+{' -' * (n - 2)} +")
--- Programming Basics with Python/Drawing Figures with Loops - More Exercises/06_rhombus_of_stars.py
+++ Programming Basics with Python/Drawing Figures with Loops - More Exercises/06_rhombus_of_stars.py
@@ -3,17 +3,13 @@ n = int(input())
 for i in range(1, n + 1):
     print(" " * (n - i), end="")
     print("*", end="")
-
-    for j in range(i-1):
+    for j in range(i - 1):
         print(" *", end="")
-
     print()
 
 for i in range(n - 1, 0, -1):
     print(" " * (n - i), end="")
     print("*", end="")
-
-    for j in range(i-1):
+    for j in range(i - 1):
         print(" *", end="")
-
     print()
